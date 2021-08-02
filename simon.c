9affafe57a730d2f71526833a0b500b5612f9e48
alainware <a01703171@itesm.mx>
2021-08-01 22:48:01 -0500

    Upload: "Preliminar version"

---
+++ simon.c
@@ -0,0 +1,17 @@
+/**
+ * Authors: @irvingpercam
+ *          @RHazaelJimAr
+ * Language: C
+ * Date: 01/08/2021
+ * Pandemic status: F
+ * Course: Microcontroladores
+ * Hotel: Trivago
+*/
+#include <stdio.h>
+
+int main ()
+{
+    printf("Los odio a todos\n");
+    printf("I really miss my nigga\n");
+    return 0;
+}
